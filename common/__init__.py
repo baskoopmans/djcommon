1f933e828db6f77daa299a35c78f3bd8c6921f0c
Bas Koopmans <e-mail@baskoopmans.nl>
2011-11-11 15:43:48 +0100

    Template library common and development are loaded automatically

--- common/__init__.py
+++ common/__init__.py
@@ -1,2 +1,7 @@
 VERSION = (0, 0, 0)
 __version__ = '.'.join(map(str, VERSION))
+
+
+from django import template
+template.add_to_builtins('common.templatetags.common')
+template.add_to_builtins('common.templatetags.development')
