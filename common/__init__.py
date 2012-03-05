798c4679fdc9ab33a27dab9c1b0a201ea2c29366
Bas Koopmans <bas@baksteenmedia.nl>
2012-03-05 16:53:32 +0100

    Removed options db_name in init

--- common/__init__.py
+++ common/__init__.py
@@ -4,7 +4,3 @@ __version__ = '.'.join(map(str, VERSION))
 from django import template
 template.add_to_builtins('common.templatetags.common')
 template.add_to_builtins('common.templatetags.development')
-
-# Add db_name to options for use in model.Meta class
-from django.db.models import options
-options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('db_name',)
