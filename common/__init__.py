354fb43cc95d68b06b85e8d1fa2426ca663ef8b9
Bas Koopmans <bas@baksteenmedia.nl>
2012-03-05 16:47:46 +0100

    Add db_name to options for use in model.Meta class

--- common/__init__.py
+++ common/__init__.py
@@ -1,7 +1,10 @@
-VERSION = (0, 0, 0)
+VERSION = (0, 1, 0)
 __version__ = '.'.join(map(str, VERSION))
-
 
 from django import template
 template.add_to_builtins('common.templatetags.common')
 template.add_to_builtins('common.templatetags.development')
+
+# Add db_name to options for use in model.Meta class
+import django.db.models.options as options
+options.DEFAULT_NAMES = options.DEFAULT_NAMES + ('db_name',)
