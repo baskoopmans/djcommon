733b901f9c166099f2e298d6706d4b6ddad922aa
Bas Koopmans <bas@baksteenmedia.nl>
2012-04-02 17:37:28 +0200

    Added ForceResponseMiddleware

--- common/middleware.py
+++ common/middleware.py
@@ -7,6 +7,33 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponseForbidden, get_host
 
 
+class ForceResponse(Exception):
+    def __init__(self, response):
+        self.response = response
+
+
+class ForceResponseMiddleware:
+    def process_exception(self, request, e):
+        """
+		Based on http://djangosnippets.org/snippets/2541/
+		
+		Because django plugins cannot throw raw response(redirect is required to user dashboard after form is submitted),
+        a solution is to raise an exception, catch it with middleware and react.
+
+        This middleware checks for ForceResponse exception and returns it's response object.
+		
+        Instance of TemplateSyntaxError has exc_info field where it has the original exception. exc_info[1] is the exception instance.
+        """
+        from django.template import TemplateSyntaxError
+        if isinstance(e, TemplateSyntaxError) and getattr(e, 'exc_info', 0):
+            try:
+                e = e.exc_info[1]
+            except: # Not iterable or IndexError
+                raise e # as if nothing had happened
+        if isinstance(e, ForceResponse):
+            return e.response
+			
+
 class StripWhitespaceMiddleware:
     """
     Strips leading and trailing whitespace from response content.
