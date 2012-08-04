5d6903b606f1395097d7aba43eaf8f989b34c0b5
Bas <bas@baksteenmedia.nl>
2012-08-04 12:51:55 +0200

    Added OrderableModel, static for admin ordering

--- common/models.py
+++ common/models.py
@@ -3,6 +3,29 @@
 from django.db import models
 
 
+class OrderableModel(models.Model):
+    """
+    Add extra field and default ordering column for and inline orderable model
+    """
+    position = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('position',)
+
+    def save(self, force_insert=False, force_update=False, using=None):
+        """
+        Calculate position (max+1) for new records
+        """
+        if not self.inline_ordering_position:
+            max = self.__class__.objects.filter().aggregate(models.Max('position'))
+            try:
+                self.position = max['position__max'] + 1
+            except TypeError:
+                self.position = 1
+        return super(OrderableModel, self).save(force_insert=force_insert, force_update=force_update, using=using)
+
+
 class TimeStampedModel(models.Model):
   date_created = models.DateTimeField(auto_now_add=True)
   date_modified = models.DateTimeField(auto_now=True)
---
+++ common/static/js/ordering_inlines.js
@@ -0,0 +1,23 @@
+jQuery(function($) {
+    $("div.inline-group").sortable({
+        axis: 'y',
+        placeholder: 'ui-state-highlight',
+        forcePlaceholderSize: 'true',
+        items: '.row1, .row2',
+        update: update
+    });
+    $("div.inline-group").disableSelection();
+});
+
+function update() {
+    $('.row1, .row2').each(function(i) {
+        $(this).find('input[id$=position]').val(i+1);
+    });
+}
+
+jQuery(document).ready(function($){
+    $(this).find('.original').hide();
+    $(this).find('input[id$=position]').parent('td').hide().parent('tr').parent('tbody').parent('table').find("th:contains('Position')").hide();
+    $(this).find('input[id$=position]').parent('td').hide().parent('tr').parent('tbody').parent('table').css('cursor','move');
+    $('.add-row a').click(update);
+});
