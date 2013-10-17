36c5493bb8075b4f83674e6c917aab3266ce9eed
Bas <bas@baksteenmedia.nl>
2013-10-17 15:59:50 +0200

    Fixed required Meta bug in EnhancedForm

--- common/forms.py
+++ common/forms.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext as _
 from django.utils.functional import lazy
 from django.utils.safestring import mark_safe
 
-from .helpers import uniqify_list
+from .helpers import uniqify_list, camelcase_to_underscores
 
 def add_css_classes(f, **kwargs):
     """
@@ -30,57 +30,61 @@ class CombinedFieldsMixin(object):
     def __init__(self, *args, **kwargs):
         super(CombinedFieldsMixin, self).__init__(*args, **kwargs)
 
-        # when subclassing a subclassed form with extra fields
-        # remove those fields when the field is in exclude list
-        for field_name in getattr(self.Meta, 'exclude', ()):
-            if self.fields.has_key(field_name):
-                del self.fields[field_name]
+        if hasattr(self, 'Meta'):
 
-        # create combined_fields
-        self.combined_fields = getattr(self.Meta, 'combined_fields', ())
+            # when subclassing a subclassed form with extra fields
+            # remove those fields when the field is in exclude list
+            for field_name in getattr(self.Meta, 'exclude', ()):
+                if self.fields.has_key(field_name):
+                    del self.fields[field_name]
 
-        for combination in self.combined_fields:
-            _label_set = []
-            _css_classes_set = []
-            _first_field = None
+            # create combined_fields
+            self.combined_fields = getattr(self.Meta, 'combined_fields', ())
 
-            for counter, field_name in enumerate(combination):
-                field = self.fields[field_name]
-                field.combined = { 'first': False, 'last': False }
+            for combination in self.combined_fields:
+                _label_set = []
+                _css_classes_set = []
+                _first_field = None
 
-                #_css_classes_set.append(self[field_name].css_classes())
+                for counter, field_name in enumerate(combination):
+                    field = self.fields[field_name]
+                    field.combined = { 'first': False, 'last': False }
 
-                if counter == 0:
-                    _first_field = field
-                    field.combined['first'] = True
-                elif counter == len(combination) - 1:
-                    field.combined['last'] = True
+                    #_css_classes_set.append(self[field_name].css_classes())
 
-                _label_set.append(u"%s" % field.label)
+                    if counter == 0:
+                        _first_field = field
+                        field.combined['first'] = True
+                    elif counter == len(combination) - 1:
+                        field.combined['last'] = True
 
-            _first_field.combined['label'] = u" + ".join(uniqify_list(_label_set, preserve_order=True))
-            _first_field.combined['label'] = _first_field.combined['label']
-            _first_field.combined['errors'] = ErrorDict()
-            _first_field.combined['css_classes'] = ' '.join(_css_classes_set)
+                    _label_set.append(u"%s" % field.label)
+
+                _first_field.combined['label'] = u" + ".join(uniqify_list(_label_set, preserve_order=True))
+                _first_field.combined['label'] = _first_field.combined['label']
+                _first_field.combined['errors'] = ErrorDict()
+                _first_field.combined['css_classes'] = ' '.join(_css_classes_set)
 
     def _post_clean(self, *args, **kwargs):
         super(CombinedFieldsMixin, self)._post_clean(*args, **kwargs)
 
-        self.combined_fields = getattr(self.Meta, 'combined_fields', ())
+        if hasattr(self, 'Meta'):
 
-        for combination in self.combined_fields:
-            _error_set = []
-            _first_field = None
+            self.combined_fields = getattr(self.Meta, 'combined_fields', ())
 
-            for counter, field_name in enumerate(combination):
-                field = self.fields[field_name]
+            for combination in self.combined_fields:
+                _error_set = []
+                _first_field = None
 
-                if counter == 0:
-                    _first_field = field
+                for counter, field_name in enumerate(combination):
+                    field = self.fields[field_name]
 
-                _error_set += self.errors.get(field_name, [])
+                    if counter == 0:
+                        _first_field = field
 
-            _first_field.combined['errors'] = ErrorList(uniqify_list(_error_set, True))
+                    _error_set += self.errors.get(field_name, [])
+
+                _first_field.combined['errors'] = ErrorList(uniqify_list(_error_set, True))
 
 
 class EnhancedForm(CombinedFieldsMixin, forms.Form):
@@ -88,8 +92,18 @@ class EnhancedForm(CombinedFieldsMixin, forms.Form):
     required_css_class = 'required'
     formfield_callback = add_css_classes
 
+    @property
+    def name(self):
+        return camelcase_to_underscores(self.__class__.__name__)
+
+
 class EnhancedModelForm(CombinedFieldsMixin, forms.ModelForm):
     error_css_class = 'error'
     required_css_class = 'required'
     formfield_callback = add_css_classes
+
+    @property
+    def name(self):
+        return camelcase_to_underscores(self.__class__.__name__)
+
 
